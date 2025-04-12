0506200650377f689ba30ea88920f71bf4ab3c09
Eben <kwabenat679@gmail.com>
2025-04-12 11:30:23 +0000

    hello test

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print("Hello, World! 😊")
+
+#prints hello world 10 times
+print("Hello, World! 😊" * 10)
+
+#prints hello world 20 times
+print("Hello, World! 😊" * 10 + "Hello, World! 😊" * 10)
+
+print(1) # This is a comment after code
